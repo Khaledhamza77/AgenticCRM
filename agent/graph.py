37814198ba082ca74c766ef119d3f0ba72c14f8b
Khaled Ibrahim <khaledhamza@aucegypt.edu>
2025-06-28 16:57:44 +0300

    hotfix

--- agent/graph.py
+++ agent/graph.py
@@ -5,22 +5,20 @@ from langgraph.graph import StateGraph, END
 from ..messages import EmailMessage, WhatsappMessage
 
 class Graph:
-    def __init__(self, llm, outgoing_mailbox):
+    def __init__(self, outgoing_mailbox):
         logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
-
-        self.llm = llm
         self.outgoing_mailbox = outgoing_mailbox
     
     def email_ingestion_node(self, state: ClassifierAgentState) -> ClassifierAgentState:
         try:
-            with open(self.llm.state['path'], "r") as f:
+            with open(state['path'], "r") as f:
                 lines = f.readlines()
             
             for line in lines:
                 line = line.strip()
                 if line.startswith("message:"):
                     type = line.split(":")[1].strip()
-                    self.llm.state['type'] = type
+                    state['type'] = type
                 elif line.startswith("sender:"):
                     sender = line.split(":")[1].strip()
                 elif line.startswith("subject:"):
@@ -47,23 +45,23 @@ class Graph:
                     sender=sender,
                     timestamp=timestamp
                 )
-            self.llm.state['message'] = message
-            self.llm.state['action'] = f"Message from {message.sender} ingested successfully."
+            state['message'] = message
+            state['action'] = f"Message from {message.sender} ingested successfully."
             logging.info(f"Message from {message.sender} ingested successfully.")
-            return self.llm.state
+            return state
         except Exception as e:
             logging.error(f"Failed to ingest email: {e}")
             raise ValueError(f"Email ingestion is not enabled: {e}")
     
     def send_email_node(self, state: ClassifierAgentState) -> ClassifierAgentState:
-        if self.llm.state['classification_result'].category != "Spam":
+        if state['classification_result'].category != "Spam":
             filename = str(uuid.uuid4())
             with open(f"{self.outgoing_mailbox}/{filename}.txt", "w") as f:
-                f.write(f"To: {self.llm.state['message'].sender}\n")
-                f.write(f"Subject: {self.llm.state['classification_result'].category} Query Automated Response\n")
-                f.write(f"Body: {self.llm.state['response']}\n")
-            self.llm.state['action'] = f"Email sent to {self.llm.state['message'].sender} with filename {filename}.txt"
-            return self.llm.state
+                f.write(f"To: {state['message'].sender}\n")
+                f.write(f"Subject: {state['classification_result'].category} Query Automated Response\n")
+                f.write(f"Body: {state['response']}\n")
+            state['action'] = f"Email sent to {state['message'].sender} with filename {filename}.txt"
+            return state
     
     def build(self) -> StateGraph:
         workflow = StateGraph(ClassifierAgentState)
